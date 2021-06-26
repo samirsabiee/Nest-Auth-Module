72768a71cbb91dfdddd010cb45c91e44d2ab9a71
samir sabiee <samirsabiee@gmail.com>
2021-06-26 12:51:00 +0430

    connect to mongodb by typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      host: 'localhost',
+      port: 27017,
+      database: 'nest-auth-module',
+      useNewUrlParser: true,
+      autoLoadEntities: true,
+      useUnifiedTopology: true,
+      entities: [join(__dirname, '**/**/**.entity{.ts,.js}')],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
